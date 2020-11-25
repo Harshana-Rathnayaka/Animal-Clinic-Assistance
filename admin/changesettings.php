dddb45bf687e2b1754b1a8a033c569369b4e7913
Harshana ratnayaka <huratnayaka@gmail.com>
2020-11-26 02:45:42 +0530

    dynamic footer

--- admin/changesettings.php
+++ admin/changesettings.php
@@ -146,13 +146,7 @@ include 'sidebar.php';
                     </main>
 
                     <div class="ml-auto mr-auto text-center py-5 mt-5">
-                        <footer class="footer">
-                            <div class="footer__block block no-margin-bottom">
-                                <div class="container-fluid text-center">
-                                    <p class="no-margin-bottom">2020 &copy; Dreeko Corporations | All Rights Reserved. <a title="www.github.com/Harshana-Rathnayaka" target="_blank" href="https://github.com/Harshana-Rathnayaka" class="icon-repo-forked"> Repository &rightarrowtail;</a></p>
-                                </div>
-                            </div>
-                        </footer>
+                    <?php include 'footer.php';?>
                     </div>
 
                 </div>
--- admin/footer.php
+++ admin/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
               <div class="footer__block block no-margin-bottom">
                 <div class="container-fluid text-center">
-                  <p class="no-margin-bottom">2020 &copy; Dreeko Corporations | All Rights Reserved. <a title="www.github.com/Harshana-Rathnayaka" target="_blank" href="https://github.com/Harshana-Rathnayaka" class="icon-repo-forked"> Repository &rightarrowtail;</a></p>
+                  <p class="no-margin-bottom">2020 &copy; Dreeko Corporations | All Rights Reserved. <a title="https://github.com/Harshana-Rathnayaka/Animal-Clinic-Assistance" target="_blank" href="https://github.com/Harshana-Rathnayaka/Animal-Clinic-Assistance" class="icon-repo-forked"> Repository &rightarrowtail;</a></p>
                 </div>
               </div>
             </footer>
